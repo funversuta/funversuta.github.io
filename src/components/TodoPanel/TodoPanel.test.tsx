097a8fe9a8550b75c84303eba3ceca8a84f8d29c
funversuta <funversuta@gmail.com>
2023-06-13 22:54:51 +0700

    test tsx

--- src/components/TodoPanel/TodoPanel.test.tsx
+++ src/components/TodoPanel/TodoPanel.test.tsx
@@ -5,15 +5,15 @@ import { TodoPanel } from './TodoPanel';
 
 describe('Form', () => {
   it('input event', async () => {
-    render(<TodoPanel />);
+    render(<TodoPanel addTodo={function ({ name }: Omit<Todo, 'id' | 'checked'>): void {}} />);
     const input = screen.getByPlaceholderText(/What needs to be done?/i);
     expect(screen.queryByTestId('value-test')).toContainHTML('');
     await userEvent.type(input, 'Тест');
     expect(screen.queryByTestId('value-test')).toContainHTML('Тест');
   }),
     it('keyDown event', () => {
-      render(<TodoPanel />);
-      const button = screen.queryByTestId('value-test');
+      render(<TodoPanel addTodo={function ({ name }: Omit<Todo, 'id' | 'checked'>): void {}} />);
+      const button = screen.queryByTestId('value-test') as Element;
       expect(screen.queryByTestId('submit'));
       fireEvent.keyPress(button);
       expect(screen.queryByTestId('value-test'));
